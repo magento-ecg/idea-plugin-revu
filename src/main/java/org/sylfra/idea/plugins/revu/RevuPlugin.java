89d8509b9608dec0c5e3bce76a2f8b1cc3f11553
emelnyk <emelnyk@ebay.com>
2016-04-21 13:10:04 +0300

    Keeping pointer on selected issue

--- src/main/java/org/sylfra/idea/plugins/revu/RevuPlugin.java
+++ src/main/java/org/sylfra/idea/plugins/revu/RevuPlugin.java
@@ -16,61 +16,50 @@ import java.io.File;
  * @author <a href="mailto:syllant@gmail.com">Sylvain FRANCOIS</a>
  * @version $Id$
  */
-public class RevuPlugin implements ProjectComponent
-{
-  public static final String PLUGIN_NAME = "reVu";
+public class RevuPlugin implements ProjectComponent {
+    public static final String PLUGIN_NAME = "reVu";
 
-  /**
-   * {@inheritDoc}
-   */
-  @NonNls
-  @NotNull
-  public String getComponentName()
-  {
-    return PLUGIN_NAME;
-  }
+    /**
+     * {@inheritDoc}
+     */
+    @NonNls
+    @NotNull
+    public String getComponentName() {
+        return PLUGIN_NAME;
+    }
 
-  /**
-   * {@inheritDoc}
-   */
-  public void initComponent()
-  {
-    UIUtil.invokeLaterIfNeeded(new Runnable()
-    {
-      public void run()
-      {
-        ApplicationManager.getApplication().runWriteAction(new Runnable()
-        {
-          public void run()
-          {
-            addSchemaResource("1_0");
-          }
+    /**
+     * {@inheritDoc}
+     */
+    public void initComponent() {
+        UIUtil.invokeLaterIfNeeded(new Runnable() {
+            public void run() {
+                ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                    public void run() {
+                        addSchemaResource("1_0");
+                    }
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  private void addSchemaResource(String version)
-  {
-    File file = new File(
-      getClass().getResource("/org/sylfra/idea/plugins/revu/resources/schemas/revu_" + version + ".xsd").getFile());
-    ExternalResourceManager.getInstance().addResource(
-      "http://plugins.intellij.net/revu/ns/revu_" + version + ".xsd",
-      file.getAbsolutePath());
-  }
+    private void addSchemaResource(String version) {
+        File file = new File(
+                getClass().getResource("/org/sylfra/idea/plugins/revu/resources/schemas/revu_" + version + ".xsd").getFile());
+        ExternalResourceManager.getInstance().addResource(
+                "http://plugins.intellij.net/revu/ns/revu_" + version + ".xsd",
+                file.getAbsolutePath());
+    }
 
-  /**
-   * {@inheritDoc}
-   */
-  public void disposeComponent()
-  {
-  }
+    /**
+     * {@inheritDoc}
+     */
+    public void disposeComponent() {
+    }
 
-  public void projectOpened()
-  {
-  }
+    public void projectOpened() {
+    }
 
-  public void projectClosed()
-  {
-  }
+    public void projectClosed() {
+    }
 }
--- src/main/java/org/sylfra/idea/plugins/revu/actions/issue/CreateIssueAction.java
+++ src/main/java/org/sylfra/idea/plugins/revu/actions/issue/CreateIssueAction.java
@@ -29,135 +29,123 @@ import java.util.Map;
  * @author <a href="mailto:syllant@gmail.com">Sylvain FRANCOIS</a>
  * @version $Id$
  */
-public class CreateIssueAction extends AbstractIssueAction
-{
+public class CreateIssueAction extends AbstractIssueAction {
 
-  protected String[] _targetPsiElementTypes = {"Class", "Class method"};
+    protected String[] _targetPsiElementTypes = {"Class", "Class method"};
 
 
-  @Override
-  public void actionPerformed(AnActionEvent e)
-  {
-    Project project = e.getData(PlatformDataKeys.PROJECT);
-    Editor editor = e.getData(PlatformDataKeys.EDITOR);
-    VirtualFile vFile = RevuUtils.getVirtualFile(e);
-    PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        VirtualFile vFile = RevuUtils.getVirtualFile(e);
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
 
-    if ((project == null) || (vFile == null))
-    {
-      return;
-    }
-
-    Issue issue = new Issue();
-    issue.setFile(vFile);
-    if (editor != null)
-    {
-      Document document = editor.getDocument();
-      int lineStart = document.getLineNumber(editor.getSelectionModel().getSelectionStart());
-      int lineEnd = document.getLineNumber(editor.getSelectionModel().getSelectionEnd());
-
-      Collection<PsiNamedElement> phpClassImpls = PsiTreeUtil.collectElementsOfType(psiFile, PsiNamedElement.class);
-      Map<String, ArrayList<String>> foundPsiElements = getPsiElementsByTypes(e, this._targetPsiElementTypes);
-
-      issue.setLineStart(lineStart);
-      issue.setLineEnd(lineEnd);
-      CharSequence fragment = document.getCharsSequence().subSequence(document.getLineStartOffset(lineStart),
-        document.getLineEndOffset(lineEnd));
-      issue.setHash(fragment.toString().hashCode());
-      issue.setSnippet(fragment.toString());
-      issue.setClassName(foundPsiElements.get("Class").toString().replace("[", "").replace("]", ""));
-      issue.setMethodName(foundPsiElements.get("Class method").toString().replace("[", "").replace("]", ""));
-
-    }
-    issue.setStatus(IssueStatus.TO_RESOLVE);
-
-    IssueDialog dialog = new IssueDialog(project, true);
-    dialog.show(issue);
-    if ((dialog.isOK()) && dialog.updateData(issue))
-    {
-      Review review = issue.getReview();
-
-      assert (RevuUtils.getCurrentUserLogin() != null) : "Login should be set";
-
-      issue.setHistory(RevuUtils.buildHistory(review));
-
-      if (RevuVcsUtils.fileIsModifiedFromVcs(project, vFile))
-      {
-        issue.setLocalRev(String.valueOf(System.currentTimeMillis()));
-      }
-
-      if (issue.getFile() != null)
-      {
-        VcsRevisionNumber vcsRev = RevuVcsUtils.getVcsRevisionNumber(project, issue.getFile());
-        if (vcsRev != null)
-        {
-          issue.setVcsRev(vcsRev.toString());
-        }
-      }
-
-      review.addIssue(issue);
-
-      ReviewManager reviewManager = project.getComponent(ReviewManager.class);
-      reviewManager.saveSilently(review);
-    }
-  }
-
-  @Override
-  public void update(AnActionEvent e)
-  {
-    boolean enabled = false;
-    Project project = e.getData(PlatformDataKeys.PROJECT);
-    VirtualFile vFile = RevuUtils.getVirtualFile(e);
-
-    if ((project != null) && (vFile != null))
-    {
-      Collection<Review> reviews = RevuUtils.getActiveReviewsForCurrentUser(project);
-      for (Review review : reviews)
-      {
-        User user = RevuUtils.getCurrentUser(review);
-        if (user == null)
-        {
-          continue;
+        if ((project == null) || (vFile == null)) {
+            return;
         }
 
-        boolean mayReview = RevuUtils.isActive(review) && user.hasRole(User.Role.REVIEWER);
-        if ((mayReview || (user.hasRole(User.Role.ADMIN))))
-        {
-          enabled = true;
-          break;
+        Issue issue = new Issue();
+        issue.setFile(vFile);
+        if (editor != null) {
+            Document document = editor.getDocument();
+            int lineStart = document.getLineNumber(editor.getSelectionModel().getSelectionStart());
+            int lineEnd = document.getLineNumber(editor.getSelectionModel().getSelectionEnd());
+
+            Collection<PsiNamedElement> phpClassImpls = PsiTreeUtil.collectElementsOfType(psiFile, PsiNamedElement.class);
+            Map<String, ArrayList<String>> foundPsiElements = getPsiElementsByTypes(e, this._targetPsiElementTypes);
+
+            issue.setLineStart(lineStart);
+            issue.setLineEnd(lineEnd);
+            CharSequence fragment = document.getCharsSequence().subSequence(document.getLineStartOffset(lineStart),
+                    document.getLineEndOffset(lineEnd));
+            issue.setHash(fragment.toString().hashCode());
+            issue.setSnippet(fragment.toString());
+            issue.setClassName(foundPsiElements.get("Class").toString().replace("[", "").replace("]", ""));
+            issue.setMethodName(foundPsiElements.get("Class method").toString().replace("[", "").replace("]", ""));
+
         }
-      }
+        issue.setStatus(IssueStatus.TO_RESOLVE);
+
+        IssueDialog dialog = new IssueDialog(project, true);
+        dialog.show(issue);
+        if ((dialog.isOK()) && dialog.updateData(issue)) {
+            Review review = issue.getReview();
+
+            assert (RevuUtils.getCurrentUserLogin() != null) : "Login should be set";
+
+            issue.setHistory(RevuUtils.buildHistory(review));
+
+            if (RevuVcsUtils.fileIsModifiedFromVcs(project, vFile)) {
+                issue.setLocalRev(String.valueOf(System.currentTimeMillis()));
+            }
+
+            if (issue.getFile() != null) {
+                VcsRevisionNumber vcsRev = RevuVcsUtils.getVcsRevisionNumber(project, issue.getFile());
+                if (vcsRev != null) {
+                    issue.setVcsRev(vcsRev.toString());
+                }
+            }
+
+            review.addIssue(issue);
+
+            ReviewManager reviewManager = project.getComponent(ReviewManager.class);
+            reviewManager.saveSilently(review);
+        }
     }
 
-    e.getPresentation().setEnabled(enabled);
-  }
-  protected Map<String, ArrayList<String>> getPsiElementsByTypes(AnActionEvent e, String[] inputTypes) {
-    PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
-    Editor editor = e.getData(PlatformDataKeys.EDITOR);
-    Document document = editor.getDocument();
+    @Override
+    public void update(AnActionEvent e) {
+        boolean enabled = false;
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        VirtualFile vFile = RevuUtils.getVirtualFile(e);
 
-    Map<String, ArrayList<String>> foundItems = new HashMap<String, ArrayList<String>>();
-    for (String itemName : inputTypes) {
-      foundItems.put(itemName, new ArrayList<String>());
+        if ((project != null) && (vFile != null)) {
+            Collection<Review> reviews = RevuUtils.getActiveReviewsForCurrentUser(project);
+            for (Review review : reviews) {
+                User user = RevuUtils.getCurrentUser(review);
+                if (user == null) {
+                    continue;
+                }
+
+                boolean mayReview = RevuUtils.isActive(review) && user.hasRole(User.Role.REVIEWER);
+                if ((mayReview || (user.hasRole(User.Role.ADMIN)))) {
+                    enabled = true;
+                    break;
+                }
+            }
+        }
+
+        e.getPresentation().setEnabled(enabled);
     }
 
-    Collection<PsiNamedElement> foundPsiElements = PsiTreeUtil.collectElementsOfType(psiFile, PsiNamedElement.class);
-    int selectionStart = document.getLineNumber(editor.getSelectionModel().getSelectionStart());
-    int selectionEnd = document.getLineNumber(editor.getSelectionModel().getSelectionEnd());
+    protected Map<String, ArrayList<String>> getPsiElementsByTypes(AnActionEvent e, String[] inputTypes) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        Document document = editor.getDocument();
 
-    for (PsiNamedElement k : foundPsiElements) {
-      int _LineStart = document.getLineNumber(k.getTextRange().getStartOffset());
-      int _LineEnd = document.getLineNumber(k.getTextRange().getEndOffset());
-
-      if (!((_LineStart > selectionEnd) || (_LineEnd < selectionStart))) {
+        Map<String, ArrayList<String>> foundItems = new HashMap<String, ArrayList<String>>();
         for (String itemName : inputTypes) {
-          if (itemName.equals(k.getNode().getElementType().toString())) {
-            foundItems.get(itemName).add(k.getName());
-          }
+            foundItems.put(itemName, new ArrayList<String>());
         }
-      }
-    }
 
-    return foundItems;
-  }
+        Collection<PsiNamedElement> foundPsiElements = PsiTreeUtil.collectElementsOfType(psiFile, PsiNamedElement.class);
+        int selectionStart = document.getLineNumber(editor.getSelectionModel().getSelectionStart());
+        int selectionEnd = document.getLineNumber(editor.getSelectionModel().getSelectionEnd());
+
+        for (PsiNamedElement k : foundPsiElements) {
+            int _LineStart = document.getLineNumber(k.getTextRange().getStartOffset());
+            int _LineEnd = document.getLineNumber(k.getTextRange().getEndOffset());
+
+            if (!((_LineStart > selectionEnd) || (_LineEnd < selectionStart))) {
+                for (String itemName : inputTypes) {
+                    if (itemName.equals(k.getNode().getElementType().toString())) {
+                        foundItems.get(itemName).add(k.getName());
+                    }
+                }
+            }
+        }
+
+        return foundItems;
+    }
 }
